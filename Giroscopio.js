1423a90d9d9bd12f31fa4463877454f9338d89cf
unknown <gabrielsilvapassos42@gmail.com>
2025-04-16 19:27:04 -0300

    odularizacao

---
+++ Giroscopio.js
@@ -0,0 +1,24 @@
+import { Fisica } from "./Fisica.js";
+const fisica = new Fisica();
+export class Giroscopio{
+    movimentoVerticalComBeta(
+        betaInicio,
+        betaFim,
+        limiteAmplitudeBeta
+      ) {
+        const impulso = fisica.calcularForcaSimples(betaInicio, betaFim);
+        if (impulso.forcaCoef >= limiteAmplitudeBeta) {
+          document.getElementById("coeficiente-forca").textContent =
+            impulso.forcaCoef.toFixed(2);
+          const trajetoria = fisica.calcularPercursoMovimentoLinearVertical(
+            impulso.forcaCoef,
+            1,
+            1
+          );
+          return { ...impulso, ...trajetoria };
+        } else {
+          document.getElementById("coeficiente-forca").textContent = "Baixo";
+        }
+        return null;
+      }
+}
---
+++ main.js
@@ -0,0 +1,97 @@
+
+import { Giroscopio } from "./Giroscopio.js";
+const giroscopio = new Giroscopio();
+let alpha = 0,
+  beta = 0,
+  gamma = 0;
+let tempo = 0;
+let ultimoFrame = performance.now();
+let tempoInterno = 0;
+let multiplicadorForca = 1;
+let betaInicial = 0;
+let acertouNesteCiclo = false;
+
+const minimoForcaAcerto = 10;
+const div = document.querySelector("#efeito-acerto");
+
+function resetarClasseVisual() {
+  div.className = "efeito-default";
+}
+
+function aplicarClasseVisual(classe) {
+  resetarClasseVisual();
+  div.classList.add(classe);
+}
+
+function loop() {
+  const agora = performance.now();
+  const delta = (agora - ultimoFrame) / 1000;
+  tempo += delta;
+
+  document.getElementById(
+    "contador-ritmo"
+  ).textContent = `${tempoInterno.toFixed(2)}s`;
+  document.getElementById("mensagem").textContent = "ACERTE NO TEMPO!";
+  if (betaInicial > beta) {
+    betaInicial = beta;
+  }
+  if (tempoInterno > 0) {
+    tempoInterno -= delta;
+
+    const movimentoVertical = giroscopio.movimentoVerticalComBeta(
+      betaInicial,
+      beta,
+      minimoForcaAcerto
+    );
+    if (movimentoVertical) {
+      if (tempoInterno < 0.3) {
+        aplicarClasseVisual("efeito-acerto-perfeito");
+        tempoInterno = movimentoVertical.tempoTotal;
+      } else if (tempoInterno < 0.5) {
+        aplicarClasseVisual("efeito-acerto-medio");
+        tempoInterno = movimentoVertical.tempoTotal;
+      } else if (tempoInterno < 0.8) {
+        aplicarClasseVisual("efeito-acerto-fraco");
+        tempoInterno = movimentoVertical.tempoTotal;
+      }
+
+      betaInicial = beta;
+    }
+  } else {
+    document.getElementById("mensagem").textContent = `TENTE NOVAMENTE!`;
+    if (betaInicial < 0) {
+      const movimentoVertical = giroscopio.movimentoVerticalComBeta(
+        betaInicial,
+        beta,
+        minimoForcaAcerto
+      );
+      if (movimentoVertical) {
+        tempoInterno = movimentoVertical.tempoTotal;
+        betaInicial = beta;
+      }
+    }
+  }
+  ultimoFrame = agora;
+  requestAnimationFrame(loop);
+}
+
+requestAnimationFrame(loop);
+
+if (window.DeviceOrientationEvent) {
+  window.addEventListener(
+    "deviceorientation",
+    function (event) {
+      alpha = parseFloat(event.alpha.toFixed(2));
+      beta = parseFloat(event.beta.toFixed(2));
+      gamma = parseFloat(event.gamma.toFixed(2));
+
+      document.getElementById("alpha").textContent = alpha;
+      document.getElementById("beta").textContent = beta;
+      document.getElementById("gamma").textContent = gamma;
+    },
+    true
+  );
+} else {
+  document.body.innerHTML +=
+    "<p>Seu navegador não suporta o evento DeviceOrientation.</p>";
+}
